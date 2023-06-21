84078f425a65863a16af4ded2ce1721983ec0b2c
JustAGuyLinux <drew@drewgriffin.com>
2023-06-21 16:48:20 -0400

    commit changes

--- custom.sh
+++ custom.sh
@@ -4,13 +4,21 @@ cd ~/Downloads
 git clone https://github.com/drewgrif/dotfiles
 cd ~/Downloads/dotfiles/
 cp -r bin/ ~/bin
-sudo cp bin/pulseaudio-control.bash /bin/
-cp .config/bspwm/ ~/.config/bspwm/
-cp .config/dk/ ~/.config/dk/
-cp .config/dunst/ ~/.config/dunst
-cp .config/picom/ ~/.config/picom/
-cp .config/polybar/ ~/.config/polybar
-cp .config/qtile/ ~/.config/qtile/
-cp .config/sxhkd ~/.config/sxhkd/
-cp .config/rofi/ ~/.config/rofi/
+chmod +x ~/bin/*
+sudo cp ~/bin/pulseaudio-control.bash /bin/
+cp .config/backgrounds/ ~/.config/
+cp .config/bspwm/ ~/.config/
+cp .config/dk/ ~/.config/
+cp .config/dunst/ ~/.config/
+cp .config/i3/ ~/.config/
+cp .config/picom/ ~/.config/
+cp .config/polybar/ ~/.config/
+cp .config/qtile/ ~/.config/
+cp .config/sxhkd/ ~/.config/
+cp .config/rofi/ ~/.config/
+
+# DWM patched with slstatus patched
+
+./resources/dwm-custom
+
 
