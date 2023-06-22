36af7862e7d959a1e50a6fc513011ac01116f313
JustAGuyLinux <drew@drewgriffin.com>
2023-06-22 08:46:35 -0400

    dwm-custom

--- custom.sh
+++ custom.sh
@@ -6,19 +6,18 @@ cd ~/Downloads/dotfiles/
 cp -r bin/ ~/bin
 chmod +x ~/bin/*
 sudo cp ~/bin/pulseaudio-control.bash /bin/
-cp .config/backgrounds/ ~/.config/
-cp .config/bspwm/ ~/.config/
-cp .config/dk/ ~/.config/
-cp .config/dunst/ ~/.config/
-cp .config/i3/ ~/.config/
-cp .config/picom/ ~/.config/
-cp .config/polybar/ ~/.config/
-cp .config/qtile/ ~/.config/
-cp .config/sxhkd/ ~/.config/
-cp .config/rofi/ ~/.config/
+\cp .config/backgrounds/ ~/.config/
+\cp .config/bspwm/ ~/.config/
+\cp .config/dk/ ~/.config/
+\cp .config/dunst/ ~/.config/
+\cp .config/i3/ ~/.config/
+\cp .config/picom/ ~/.config/
+\cp .config/polybar/ ~/.config/
+\cp .config/qtile/ ~/.config/
+\cp .config/sxhkd/ ~/.config/
+\cp .config/rofi/ ~/.config/
 
 # DWM patched with slstatus patched
-cd ~/bookworm-scripts
-./resources/dwm-custom
+bash ~/bookworm-scripts/resources/dwm-custom
 
 
