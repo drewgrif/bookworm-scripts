2e90c4f466a23c57fd0cc10bb33a4b71bacc1bc4
JustAGuyLinux <drew@drewgriffin.com>
2024-03-07 14:24:08 -0500

    update teal.sh

--- teal.sh
+++ teal.sh
@@ -1,13 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
 cd ~/Downloads
-git clone https://github.com/vinceliuice/Jasper-gtk-theme.git
-cd Jasper-gtk-theme/
-./install.sh -c dark --tweaks dracula
 
-cd ~/Downloads
 git clone https://github.com/vinceliuice/Colloid-icon-theme.git
-cd Colloid-icon-theme/
-./install.sh -t teal -s dracula
+cd Colloid-icon-theme
+./install.sh -t teal -s all
 
-cd
+cd ~/Downloads
+git clone https://github.com/vinceliuice/Colloid-gtk-theme.git
+cd Colloid-gtk-theme
+yes | ./install.sh -t teal -c dark --tweaks nord
+
+cd ~/Downloads
+git clone https://github.com/vinceliuice/Orchis-theme.git
+cd Orchis-theme
+yes | ./install.sh -t teal -c dark --tweaks nord
+yes | ./install.sh -t teal -c dark --tweaks black
