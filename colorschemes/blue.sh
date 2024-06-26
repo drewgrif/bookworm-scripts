102cceeef85a3b03a656f4cb3fadf14f963231e4
JustAGuyLinux <drew@drewgriffin.com>
2024-06-26 17:37:00 -0400

    Update blue.sh

--- colorschemes/blue.sh
+++ colorschemes/blue.sh
@@ -10,27 +10,29 @@ check_directory() {
 }
 
 # Check if Colloid-icon-theme is installed
-if ! check_directory "$HOME/.local/share/icons/Colloid-dark"; then
+if check_directory "$HOME/.local/share/icons/Colloid-dark"; then
+    echo "Colloid-icon-theme is already installed."
+else
     echo "Installing Colloid-icon-theme..."
     cd ~/Downloads || exit
     git clone https://github.com/vinceliuice/Colloid-icon-theme.git
     cd Colloid-icon-theme || exit
     ./install.sh -t default -s default
     rm -rf ~/Downloads/Colloid-icon-theme/
-else
-    echo "Colloid-icon-theme is already installed."
 fi
 
 # Check if Colloid-gtk-theme is installed
-if ! check_directory "$HOME/.themes/Colloid-Dark"; then
+if check_directory "$HOME/.themes/Colloid-Dark"; then
+    echo "Colloid-gtk-theme is already installed."
+else
     echo "Installing Colloid-gtk-theme..."
     cd ~/Downloads || exit
     git clone https://github.com/vinceliuice/Colloid-gtk-theme.git
     cd Colloid-gtk-theme || exit
     yes | ./install.sh -c dark -t default --tweaks black
     rm -rf ~/Downloads/Colloid-gtk-theme
-else
-    echo "Colloid-gtk-theme is already installed."
 fi
 
+# Copy GTK 3.0 configuration files
 \cp -r ~/bookworm-scripts/jag_dots/.config/gtk-3.0/ ~/.config/
+
