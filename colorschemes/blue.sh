0890ab820dcd2218ad41b7fcbc4ce1fd21af18ea
JustAGuyLinux <drew@drewgriffin.com>
2024-06-26 20:59:17 -0400

    Update blue.sh

--- colorschemes/blue.sh
+++ colorschemes/blue.sh
@@ -9,16 +9,22 @@ check_directory() {
     fi
 }
 
+# Function to handle errors
+handle_error() {
+    echo "Error: $1"
+    exit 1
+}
+
 # Check if Colloid-icon-theme is installed
 if check_directory "$HOME/.local/share/icons/Colloid-dark"; then
     echo "Colloid-icon-theme is already installed."
 else
     echo "Installing Colloid-icon-theme..."
-    cd ~/Downloads || exit
-    git clone https://github.com/vinceliuice/Colloid-icon-theme.git
-    cd Colloid-icon-theme || exit
-    ./install.sh -t default -s default
-    rm -rf ~/Downloads/Colloid-icon-theme/
+    cd ~/Downloads || handle_error "Could not change directory to ~/Downloads"
+    git clone https://github.com/vinceliuice/Colloid-icon-theme.git || handle_error "Failed to clone Colloid-icon-theme repository"
+    cd Colloid-icon-theme || handle_error "Could not change directory to Colloid-icon-theme"
+    ./install.sh -t default -s default || handle_error "Failed to install Colloid-icon-theme"
+    rm -rf ~/Downloads/Colloid-icon-theme/ || handle_error "Failed to remove Colloid-icon-theme directory"
 fi
 
 # Check if Colloid-gtk-theme is installed
@@ -26,20 +32,20 @@ if check_directory "$HOME/.themes/Colloid-Dark"; then
     echo "Colloid-gtk-theme is already installed."
 else
     echo "Installing Colloid-gtk-theme..."
-    cd ~/Downloads || exit
-    git clone https://github.com/vinceliuice/Colloid-gtk-theme.git
-    cd Colloid-gtk-theme || exit
-    yes | ./install.sh -c dark -t default --tweaks black
-    rm -rf ~/Downloads/Colloid-gtk-theme
+    cd ~/Downloads || handle_error "Could not change directory to ~/Downloads"
+    git clone https://github.com/vinceliuice/Colloid-gtk-theme.git || handle_error "Failed to clone Colloid-gtk-theme repository"
+    cd Colloid-gtk-theme || handle_error "Could not change directory to Colloid-gtk-theme"
+    yes | ./install.sh -c dark -t default --tweaks black || handle_error "Failed to install Colloid-gtk-theme"
+    rm -rf ~/Downloads/Colloid-gtk-theme || handle_error "Failed to remove Colloid-gtk-theme directory"
 fi
 
 # Function to write GTK 3 settings to ~/.config/gtk-3.0/settings.ini
-function write_gtk3_settings {
+write_gtk3_settings() {
     echo "[Settings]" > ~/.config/gtk-3.0/settings.ini
-    echo "gtk-theme-name=Colloid-Dark" >> ~/.config/gtk-3.0/settings.ini
-    echo "gtk-icon-theme-name=Colloid-dark" >> ~/.config/gtk-3.0/settings.ini
-    echo "gtk-font-name=Sans 10" >> ~/.config/gtk-3.0/settings.ini
-    echo "gtk-cursor-theme-name=Adwaita" >> ~/.config/gtk-3.0/settings.ini
+    echo "gtk-theme-name=\"Colloid-Dark\"" >> ~/.config/gtk-3.0/settings.ini
+    echo "gtk-icon-theme-name=\"Colloid-dark\"" >> ~/.config/gtk-3.0/settings.ini
+    echo "gtk-font-name=\"Sans 10\"" >> ~/.config/gtk-3.0/settings.ini
+    echo "gtk-cursor-theme-name=\"Adwaita\"" >> ~/.config/gtk-3.0/settings.ini
     echo "gtk-cursor-theme-size=0" >> ~/.config/gtk-3.0/settings.ini
     echo "gtk-toolbar-style=GTK_TOOLBAR_BOTH" >> ~/.config/gtk-3.0/settings.ini
     echo "gtk-toolbar-icon-size=GTK_ICON_SIZE_LARGE_TOOLBAR" >> ~/.config/gtk-3.0/settings.ini
@@ -49,11 +55,11 @@ function write_gtk3_settings {
     echo "gtk-enable-input-feedback-sounds=1" >> ~/.config/gtk-3.0/settings.ini
     echo "gtk-xft-antialias=1" >> ~/.config/gtk-3.0/settings.ini
     echo "gtk-xft-hinting=1" >> ~/.config/gtk-3.0/settings.ini
-    echo "gtk-xft-hintstyle=hintfull" >> ~/.config/gtk-3.0/settings.ini
+    echo "gtk-xft-hintstyle=\"hintfull\"" >> ~/.config/gtk-3.0/settings.ini
 }
 
 # Function to write GTK 2 settings to ~/.gtkrc-2.0
-function write_gtk2_settings {
+write_gtk2_settings() {
     echo "gtk-theme-name=\"Colloid-Dark\"" > ~/.gtkrc-2.0
     echo "gtk-icon-theme-name=\"Colloid-dark\"" >> ~/.gtkrc-2.0
     echo "gtk-font-name=\"Sans 10\"" >> ~/.gtkrc-2.0
@@ -70,7 +76,7 @@ function write_gtk2_settings {
     echo "gtk-xft-hintstyle=\"hintfull\"" >> ~/.gtkrc-2.0
 }
 
-# Check if ~/.config/gtk-3.0/settings.ini exists
+# Check and configure GTK 3 settings
 if [ -f ~/.config/gtk-3.0/settings.ini ]; then
     echo "File ~/.config/gtk-3.0/settings.ini already exists. Overwriting..."
     write_gtk3_settings
@@ -78,7 +84,7 @@ else
     write_gtk3_settings
 fi
 
-# Check if ~/.gtkrc-2.0 exists
+# Check and configure GTK 2 settings
 if [ -f ~/.gtkrc-2.0 ]; then
     echo "File ~/.gtkrc-2.0 already exists. Overwriting..."
     write_gtk2_settings
@@ -87,5 +93,3 @@ else
 fi
 
 echo "GTK and icon themes set to Colloid-Dark and Colloid-dark."
-
-
