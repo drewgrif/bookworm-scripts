ca73f564e1cf8d98e95cb41fd7e621b9fe865e2e
JustAGuyLinux <drew@drewgriffin.com>
2024-06-29 12:32:49 -0400

    update add_bashrc

--- install_scripts/add_bashrc.sh
+++ install_scripts/add_bashrc.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 
-# Function to prompt if the user wants to copy the justaguylinux .bashrc file
-prompt_copy_bashrc() {
-    echo "Do you want to copy the justaguylinux .bashrc file?"
-    echo "1. Yes"
-    echo "2. No"
-    read -r copy_choice
+echo "Would you like to overwrite your current .bashrc with the justaguylinux .bashrc? (y/n)"
+read response
 
-    case "$copy_choice" in
-        1)
-            echo "Copying .bashrc file..."
-            cp ~/bookworm-scripts/jag_dots/.bashrc ~/.bashrc
-            ;;
-        2)
-            echo "Skipping .bashrc file copy."
-            ;;
-        *)
-            echo "Invalid choice. Skipping .bashrc file copy."
-            ;;
-    esac
-    
-    # Adding a couple of line returns
-    echo -e "\n\n"
-}
+if [[ "$response" =~ ^[Yy]$ ]]; then
+    \cp ~/bookworm-scripts/jag_dots/.bashrc ~/.bashrc
+    echo "justaguylinux .bashrc has been copied to ~/.bashrc"
+elif [[ "$response" =~ ^[Nn]$ ]]; then
+    echo "No changes have been made to ~/.bashrc"
+else
+    echo "Invalid input. Please enter 'y' or 'n'."
+fi
